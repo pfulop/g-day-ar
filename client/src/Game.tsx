2e067c2fbcf6e29a1b4264145796ed5f8f25460b
Murat Corlu <muratcorlu@me.com>
2020-01-23 14:35:04 +0100

    Board component added

--- client/src/Game.tsx
+++ client/src/Game.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import {ViroARScene, ViroBox, ViroMaterials, ViroNode} from 'react-viro';
+import Board from './Board';
 
 const Game: React.FC = () => {
   return (
     <ViroARScene>
       <ViroNode>
-        <ViroBox
-          materials={['green']}
-          position={[0, 0, -0.5]}
-          scale={[0.5, 0.5, 0.01]}
-        />
-        <ViroBox position={[0, 0, -0.4]} scale={[0.01, 0.01, 0.01]} />
+        <Board/>
       </ViroNode>
     </ViroARScene>
   );
